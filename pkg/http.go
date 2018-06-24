ce0add35623277ea5073b8c2b6837fef215eebce
vlaurenzano <vincent.laurenzano@gmail.com>
2018-06-23 22:44:45 -0400

    http.go

--- pkg/http.go
+++ pkg/http.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"strconv"
 	"net/url"
+	"fmt"
 )
 
 func parsePath(path string) []string {
@@ -14,35 +15,47 @@ func parsePath(path string) []string {
 	return segments[1:]
 }
 
+//our response struct is always used to return data to the client
+//this keeps our api nice anc consistent
 type Response struct {
-	Data    Records   //if the db returns data it will be reflected here
-	Created int64     //if the db inserts data it will be reflected here
-	Updated int64     //if the db updates data it will be reflected here
-	Deleted int64     //if the db deletes data it will be reflected here
-	Message string `json:"message"`
+	Status  int     `json:"status"`  //our api status code
+	Message string  `json:"message"` //our api message
+	Data    Records `json:"data"`    //if the db returns data it will be reflected here
+	Created int64   `json:"created"` //if the db inserts data it will be reflected here
+	Updated int64   `json:"updated"` //if the db updates data it will be reflected here
+	Deleted int64   `json:"deleted"` //if the db deletes data it will be reflected here
+	Links   []Link  `json:"links"`
 }
 
+//Write our response to the client
+func (response *Response) Write(w http.ResponseWriter, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	response.Status = status
+	enc := json.NewEncoder(w)
+	enc.Encode(response)
+}
 
+type Link struct {
+	Rel    string `json:"rel"`
+	Href   string `json:"href"`
+	Method string `json:"method"`
+}
+
+//shortcut for returning just a message to our client
 func MessageResponse(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
-	m := Response{Message: message}
+	m := Response{Message: message, Status: status}
 	w.WriteHeader(status)
 	enc := json.NewEncoder(w)
 	enc.Encode(&m)
 }
 
-func ObjectResponse(w http.ResponseWriter, status int, object interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	enc := json.NewEncoder(w)
-	enc.Encode(object)
-}
-
 //gets a parameter and casts it to an int or returns given default
 //if an error occurs during casting it is returned
-func intParamOrDefault(values url.Values, param string, def int) (int,  error){
+func intParamOrDefault(values url.Values, param string, def int) (int, error) {
 	value := values.Get(param)
-	if(value != ""){
+	if (value != "") {
 		intVal, e := strconv.Atoi(value)
 		if e != nil {
 			return def, e
@@ -52,14 +65,12 @@ func intParamOrDefault(values url.Values, param string, def int) (int,  error){
 	return def, nil
 }
 
-
 //handle a get call
 //GET /resource -- returns records up to default limit at the default offset
 //GET /resource?limit=x -- returns records up to given limit at the default offset
 //GET /resource?offset=x&limit=y -- return records up to limit from given offset
 //GET /resource/id -- gets the resource at the given id
 func HandleGet(w http.ResponseWriter, r *http.Request, storage Storage) {
-
 
 	segments := parsePath(r.URL.Path)
 
@@ -70,34 +81,77 @@ func HandleGet(w http.ResponseWriter, r *http.Request, storage Storage) {
 		record = Record{"id": segments[1]}
 		resource = Resource{segments[len(segments)-2]}
 	} else {
-		resource = Resource{segments[len(segments)-1]}
-	}
-
-	params := r.URL.Query()
-
-	offset, e := intParamOrDefault(params, "offset",0)
-	if e != nil {
-		MessageResponse(w, 400,"improper value for 'offset'")
+		HandleGetMulti(w, r, storage)
 		return
 	}
 
-	limit, e := intParamOrDefault(params, "limit", Config().LimitDefault)
-	if e != nil {
-		MessageResponse(w, 400,"improper value for 'limit'")
-		return
-	}
-
-
-	result, err := storage.Read(resource, &record, offset, limit)
+	result, err := storage.Read(resource, &record, 0, 1)
 	if err != nil {
 		MessageResponse(w, err.Code, err.Message)
 	} else {
 		if len(result.Data) == 0 {
 			MessageResponse(w, 404, "no records found")
 		} else {
-			ObjectResponse(w, 200, result)
+			result.Links = append(result.Links, Link{Rel: "self", Href: "http://" + r.Host + r.RequestURI, Method: "GET"})
+			result.Write(w, 200)
 		}
 
+	}
+}
+
+func HandleGetMulti(w http.ResponseWriter, r *http.Request, storage Storage) {
+	segments := parsePath(r.URL.Path)
+	var record Record
+	var resource Resource
+	resource = Resource{segments[len(segments)-1]}
+	params := r.URL.Query()
+
+	offset, e := intParamOrDefault(params, "offset", 0)
+	if e != nil {
+		MessageResponse(w, 400, "improper value for 'offset'")
+		return
+	}
+
+	limit, e := intParamOrDefault(params, "limit", Config().LimitDefault)
+	if e != nil {
+		MessageResponse(w, 400, "improper value for 'limit'")
+		return
+	}
+
+	if limit < 1 {
+		limit = 1
+	}
+
+	if offset < 0 {
+		offset = 0
+	}
+
+	result, err := storage.Read(resource, &record, offset, limit)
+	if err != nil {
+		MessageResponse(w, err.Code, err.Message)
+	} else {
+
+		result.Links = append(result.Links, Link{"self", "http://" + r.Host + r.RequestURI, "GET"})
+
+		//todo this could be smarter
+		if offset > 0 {
+			previousPageOffset := offset - limit
+			if previousPageOffset < 0 {
+				previousPageOffset = 0
+			}
+			link := Link{Rel: "prev", Method: "GET"}
+			link.Href = fmt.Sprintf("http://%s%s?offset=%d&limit=%d", r.Host, r.URL.Path, previousPageOffset, limit)
+			result.Links = append(result.Links, link)
+		}
+
+		//todo this could be smarter
+		if len(result.Data) == limit {
+			nextPageOffset := offset + limit
+			link := Link{Rel: "next", Method: "GET"}
+			link.Href = fmt.Sprintf("http://%s%s?offset=%d&limit=%d", r.Host, r.URL.Path, nextPageOffset, limit)
+			result.Links = append(result.Links, link)
+		}
+		result.Write(w, 200)
 	}
 }
 
@@ -123,7 +177,8 @@ func HandlePut(w http.ResponseWriter, r *http.Request, storage Storage) {
 			status = 201
 			result.Message = "success"
 		}
-		ObjectResponse(w,status,result)
+		result.Write(w, status)
+
 	}
 
 }
@@ -144,7 +199,7 @@ func HandlePost(w http.ResponseWriter, r *http.Request, storage Storage) {
 	if err != nil {
 		MessageResponse(w, err.Code, err.Message)
 	} else {
-		ObjectResponse(w,200,result)
+		result.Write(w, 200)
 	}
 }
 
@@ -160,7 +215,7 @@ func HandleDelete(w http.ResponseWriter, r *http.Request, storage Storage) {
 		if result.Deleted == 0 {
 			MessageResponse(w, 404, "record not found")
 		} else {
-			ObjectResponse(w,200,result)
+			result.Write(w, 200)
 		}
 	}
 }
